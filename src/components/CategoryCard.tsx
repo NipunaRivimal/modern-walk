ed0f4d27be9b6458983f6a38dacaa17a4edb09b2
NipunaNuclei <nipuna@nuclei.tech>
2025-03-12 09:12:21 +0530

    basic category card

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategoryCard = () => {
+  return (
+    <div className="w-80 h-44 bg-teal-400 rounded-2xl shadow-lg flex items-center justify-center">
+      <h2 className="text-white text-2xl font-bold">Mens Clothing</h2>
+    </div>
+  );
+};
+
+export default CategoryCard;
