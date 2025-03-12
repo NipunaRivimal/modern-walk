f26b92883dc8b77b43d2aa1c5678e4048f31ad79
NipunaNuclei <nipuna@nuclei.tech>
2025-03-12 11:06:13 +0530

    header and layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="p-4 border-b-4 border-gray-300 text-center">
+      <h1 className="text-4xl font-bold">Modern Walk</h1>
+    </div>
+  );
+};
+
+export default Header;
